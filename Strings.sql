1b4525d09ae0b00a30a63a00b00da892ae5477b9
Tejasree-S27 <stejasree348@gmail.com>
2025-06-05 12:09:01 +0530

    Create Strings.sql

---
+++ Strings.sql
@@ -0,0 +1,12 @@
+create table str(
+    sname char(10),
+    address varchar(20),
+    info text
+);
+insert into str(sname,address,info)
+    values("Sam","Hyd","Laddy Boss"),
+    ("Hasini","Plmr","Bp patient"),
+    ("Bhavana","bgm","Silent"),
+    ("Ramya","pgnr","Confused person"),
+    ("Samantha","Hyd","Queen");
+select * from str;
