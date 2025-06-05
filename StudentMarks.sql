b5973f026720ff43eecfd2efd5bed0e6effc0aa2
Tejasree-S27 <stejasree348@gmail.com>
2025-06-05 13:01:26 +0530

    Create StudentMarks.sql

---
+++ StudentMarks.sql
@@ -0,0 +1,13 @@
+create table st(
+    sn varchar(20),
+    t int,
+    h int,
+    e int,
+    m int,
+    sc int,
+    su int,
+    avg float
+);
+insert into st values
+    ("Raju",65,66,90,75,93,(65+66+90+75+93),(65+66+90+75+93)/5);
+select * from st;
