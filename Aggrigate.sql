323cb38da666f7d6c53fddc1e11cb67954e3e42a
Tejasree-S27 <stejasree348@gmail.com>
2025-06-05 16:03:55 +0530

    Update Aggrigate.sql

--- Aggrigate.sql
+++ Aggrigate.sql
@@ -7,10 +7,10 @@ create table employee(
     es float
     );
 insert into employee values
-(101,"prem","CSE[DEAN]",9393979863,'1980-09-23',345000),
+(101,"Sam","CSE[DEAN]",9393979863,'1980-09-23',345000),
 (102,"praveen","CSE[Hod]",8093979863,'1989-09-26',250000),
 (103,"prakash","CSE[prof]",9398804083,'1994-11-30',25000),
-(101,"prem","CSE[stu]",92348906,'1965-06-22',495000);
+(104,"prem","CSE[stu]",92348906,'1965-06-22',495000);
 update employee
 set es=((es * 10)/100)+es;
 select * from employee
