95051fb4ddda7930cae760f780f350ba6fdc05f5
Tejasree-S27 <stejasree348@gmail.com>
2025-06-05 14:57:07 +0530

    Create Operator_Like.sql

---
+++ Operator_Like.sql
@@ -0,0 +1,16 @@
+create table st(
+    sno int,
+    sn varchar(20),
+    sa int
+);
+insert into st values
+    (1,"",30),
+    (2,"Sam",24),
+    (3,"Raj",33),
+    (4,"Navs",19),
+    (5,"Rekha",22);
+select * from st where sn like '';
+select * from st 
+    where sn is NULL;
+select * from st
+    where sn is not NULL;
